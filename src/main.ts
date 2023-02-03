c2923aecfd46abb67c0940be9ea9262481762814
joao francisco <joaofrancisco.guarda@gmail.com>
2023-02-03 09:15:27 -0300

    feat: creation of vue app with vuetify, Pinia, i18n and DatePicker component

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import {
+  createApp,
+  Component,
+  toRefs,
+} from "vue";
+import App from "./App.vue";
+
+import { aliases, fa } from "vuetify/iconsets/fa";
+
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createPinia } from "pinia";
+
+import * as VueI18n from "vue-i18n";
+import messages from "./translations/translations";
+
+const i18n = VueI18n.createI18n({
+  locale: localStorage.getItem(
+    "@khiza:user-locale"
+  ) as string,
+  fallbackLocale: "en",
+  messages,
+});
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+});
+
+createApp(App)
+  .use(createPinia())
+  .use(i18n)
+  .use(vuetify)
+  .component("Datepicker", Datepicker)
+  .mount("#app");
